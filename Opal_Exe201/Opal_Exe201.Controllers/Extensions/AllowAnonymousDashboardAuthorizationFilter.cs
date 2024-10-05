38caad85df49d2d5dcf95555f70bbfe6a0471b77
Nguyen Vi Tien <Nguyevitien2003@gmail.com>
2024-10-06 02:51:52 +0700

    [fix]

---
+++ Opal_Exe201/Opal_Exe201.Controllers/Extensions/AllowAnonymousDashboardAuthorizationFilter.cs
@@ -0,0 +1,12 @@
+﻿using Hangfire.Dashboard;
+
+namespace Opal_Exe201.Controllers.Extensions
+{
+    public class AllowAnonymousDashboardAuthorizationFilter : IDashboardAuthorizationFilter
+    {
+        public bool Authorize(DashboardContext context)
+        {
+            return true;
+        }
+    }
+}
